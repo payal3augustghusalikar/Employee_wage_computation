b86bbb8be163b30ef160dd98fa3141f8bbcbbd7c
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-10-09 01:35:21 +0530

    Wages calculated for 20 working days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,25 +4,32 @@ echo "Welcome to employeeWage Computation"
 
 Is_Full_Time=2
 Is_Part_Time=1
+Total_Salary=0
 Emp_Rate_Per_Hour=20
+Num_Working_Days=20
 
-empcheck=$( expr $RANDOM % 3 )
+for (( day=1; day<=$Num_Working_Days; day++ ))
+do
+	empcheck=$( expr $RANDOM % 3 )
 
-case $empcheck in
+		case $empcheck in
 
-	$Is_Full_Time)
-		emphrs=8
-			;;
+		$Is_Full_Time)
+			emphrs=8
+				;;
 
-	$Is_Part_Time)
-		emphrs=4
-			;;
+		$Is_Part_Time)
+			emphrs=4
+				;;
 
 		*)
-		emphrs=0
-esac
+			emphrs=0
+		esac
 
-		Total_Salary=$(( $Emp_Rate_Per_Hour * $emphrs ))
+		Salary=$(( $Emp_Rate_Per_Hour * $emphrs ))
 
-		echo "Daily Wage of Employee is $Total_Salary"
+		Total_Salary=$(( $Salary + $Total_Salary ))
+done
+
+	echo "Daily Wage of Employee is $Total_Salary"
 
