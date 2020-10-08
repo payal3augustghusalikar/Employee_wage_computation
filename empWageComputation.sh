51987555396e69a23583f36f19bfca4bd9ad3fb3
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-10-09 00:50:58 +0530

    Part time Hours added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,22 +2,27 @@
 
 echo "Welcome to employeeWage Computation"
 
-Is_Full_Time=1
-
+Is_Full_Time=2
+Is_Part_Time=1
 Emp_Rate_Per_Hour=20
 
-a=$( expr $RANDOM % 2 )
+empcheck=$( expr $RANDOM % 3 )
 
-	if [ $a -eq $Is_Full_Time ]
-	then
+case $empcheck in
+
+	$Is_Full_Time)
+		emphrs=10
+			;;
+
+	$Is_Part_Time)
 		emphrs=8
+			;;
 
-		Total_Salary=$(( $Emp_Rate_Per_Hour * $emphrs ));
+		*)
+		emphrs=0
+esac
+
+		Total_Salary=$(( $Emp_Rate_Per_Hour * $emphrs ))
 
 		echo "Daily Wage of Employee is $Total_Salary"
 
-	elif [ $a -eq 0 ]
-	then
-		echo "employee is absent"
-
-	fi
