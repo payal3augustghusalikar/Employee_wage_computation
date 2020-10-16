96af59fe48efbfee4739e0874b8ceb229cda9748
Payal Ghusalikar <payal.ghusalikar9@gmail.com>
2020-10-16 23:54:01 +0530

    Refactored : get total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,7 +2,8 @@
 
 echo "Welcome to employeeWage Computation"
 echo " "
-echo "daily wage of employee is:"
+echo "daily wage of employee is: "
+
 Is_Full_Time=2
 Is_Part_Time=1
 Total_Salary=0
@@ -10,41 +11,40 @@ Emp_Rate_Per_Hour=20
 Num_Working_Days=20
 Max_Month_Hrs=100
 Total_Emp_Hrs=0
-
 totalworkingdays=0
 
 function getworkinghour
 {
-		local emphrs=0;
+	local emphrs=0;
 
+	case $empcheck in
 
-			case $empcheck in
+		$Is_Full_Time)
+			emphrs=8
+			;;
 
-			$Is_Full_Time)
-				emphrs=8
-					;;
+		$Is_Part_Time)
+			emphrs=4
+			;;
 
-			$Is_Part_Time)
-				emphrs=4
-					;;
-
-			*)
-				emphrs=0
-			esac
-echo $emphrs
+		*)
+			emphrs=0
+	esac
+	echo $emphrs
 }
+
 while [[ $Total_Emp_Hrs -lt $Max_Month_Hrs && $totalworkingdays -lt $Num_Working_Days ]]
 do
-		((TotalWorkingDays++))
-      empcheck=$( expr $RANDOM % 3 )
+	((TotalWorkingDays++))
+   empcheck=$( expr $RANDOM % 3 )
 
-		emphrs=$( getworkinghour $empcheck )
+	emphrs=$( getworkinghour $empcheck )
 
-		Salary=$(( $Emp_Rate_Per_Hour * $emphrs ))
+	Salary=$(( $Emp_Rate_Per_Hour * $emphrs ))
 
-		Total_Salary=$(( $Salary + $Total_Salary ))
+	Total_Salary=$(( $Salary + $Total_Salary ))
 
-		Total_Emp_Hrs=$(( $Total_Emp_Hrs + $emphrs ))
+	Total_Emp_Hrs=$(( $Total_Emp_Hrs + $emphrs ))
 
 	echo " Day $TotalWorkingDays ==> $Salary"
 
